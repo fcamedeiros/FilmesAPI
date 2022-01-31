d1be95407dfb5fa89fc3f7b447cbde3aa9113fd0
Fabricio Coutinho de Medeiros <fcamedeiros@gmail.com>
2022-01-31 18:10:06 -0300

    adicionando o método post para adicionar filmes

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+        }
+    }
+}
